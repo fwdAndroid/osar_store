386f748a196d4253ef7b9c178f09807618068746
fwdAndroid <fwdkaleem@gmail.com>
2023-05-02 23:40:29 +0500

    MainAuth UI Done

--- lib/auth/main_auth.dart
+++ lib/auth/main_auth.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -11,6 +12,26 @@ class MainAuth extends StatefulWidget {
 class _MainAuthState extends State<MainAuth> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              "Let's Get Started",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30),
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.only(left: 30, right: 30, bottom: 50),
+              child: Image.asset("assets/google.png"))
+        ],
+      ),
+    );
   }
 }
