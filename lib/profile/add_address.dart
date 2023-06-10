1bdf232905d8e6c57e12257b3f596f4aa07b27f0
fawad kaleem <fwdkaleem@gmail.com>
2023-06-10 18:26:04 +0500

    Changes

--- lib/profile/add_address.dart
+++ lib/profile/add_address.dart
@@ -5,7 +5,9 @@ import 'package:flutter_google_places_hoc081098/flutter_google_places_hoc081098.
 import 'package:google_api_headers/google_api_headers.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_maps_webservice/places.dart';
+import 'package:osar_store/status/geo.dart';
 import 'package:osar_store/status/user_status.dart';
+import 'package:geocoding/geocoding.dart';
 
 class AddAddress extends StatefulWidget {
   @override
@@ -13,138 +15,203 @@ class AddAddress extends StatefulWidget {
 }
 
 class _AddAddressState extends State<AddAddress> {
-  String googleApikey = "AIzaSyAXlAJ4VBgBIle9-nP-XYcmcL2APpuHN2U";
+  String googleApikey = "AIzaSyBffT8plN_Vdcd308KgmzIfGVQN6q-CkAo";
   GoogleMapController? mapController; //contrller for Google map
   CameraPosition? cameraPosition;
-  LatLng startLocation = LatLng(27.6602292, 85.308027);
-  String location = "Search Location";
+  bool _isLoading = false;
+  List latlong = [];
+  String location = 'Please move map to A specific location.';
   TextEditingController _addrController = TextEditingController();
+  @override
+  void initState() {
+    getLatLong();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _addrController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(children: [
-      GoogleMap(
-        //Map widget from google_maps_flutter package
-        zoomGesturesEnabled: true, //enable Zoom in, out on map
-        initialCameraPosition: CameraPosition(
-          //innital position in map
-          target: startLocation, //initial position
-          zoom: 14.0, //initial zoom level
-        ),
-        mapType: MapType.normal, //map type
-        onMapCreated: (controller) {
-          //method called when map is created
-          setState(() {
-            mapController = controller;
-          });
-        },
-        // onCameraIdle: () async {
-        //   //when map drag stops
-        //   List<Placemark> placemarks = await placemarkFromCoordinates(
-        //       cameraPosition!.target.latitude,
-        //       cameraPosition!.target.longitude);
-        //   setState(() {
-        //     //get place name from lat and lang
-        //     location = placemarks.first.administrativeArea.toString() +
-        //         ", " +
-        //         placemarks.first.street.toString();
-        //     _addrController.text = location;
-        //   });
-        // },
-      ),
+    LatLng startLocation = _isLoading
+        ? const LatLng(25.276987, 55.296249)
+        : LatLng(latlong[0], latlong[1]);
+    return _isLoading
+        ? const Center(
+            child: CircularProgressIndicator.adaptive(),
+          )
+        : Column(
+            children: [
+              Container(
+                height: 680,
+                child: Stack(
+                  children: [
+                    GoogleMap(
+                      //Map widget from google_maps_flutter package
+                      zoomGesturesEnabled: true, //enable Zoom in, out on map
+                      initialCameraPosition: CameraPosition(
+                        //innital position in map
+                        target: startLocation, //initial position
+                        zoom: 14.0, //initial zoom level
+                      ),
+                      mapType: MapType.normal, //map type
+                      onMapCreated: (controller) {
+                        //method called when map is created
+                        setState(() {
+                          mapController = controller;
+                        });
+                      },
+                      onCameraMove: (CameraPosition cameraPositiona) {
+                        cameraPosition = cameraPositiona; //when map is dragging
+                      },
+                      onCameraIdle: () async {
+                        List<Placemark> addresses =
+                            await placemarkFromCoordinates(
+                                cameraPosition!.target.latitude,
+                                cameraPosition!.target.longitude);
 
-      //search autoconplete input
-      Positioned(
-        //search input bar
-        top: 10,
-        child: InkWell(
-          onTap: () async {
-            var place = await PlacesAutocomplete.show(
-                context: context,
-                apiKey: googleApikey,
-                mode: Mode.overlay,
-                types: [],
-                strictbounds: false,
-                components: [Component(Component.country, 'pk')],
-                //google_map_webservice package
-                onError: (err) {
-                  print(err);
-                });
+                        var first = addresses.first;
+                        print("${first.name} : ${first..administrativeArea}");
 
-            if (place != null) {
-              setState(() {
-                location = place.description.toString();
-                _addrController.text = location;
-              });
+                        List<Placemark> placemarks =
+                            await placemarkFromCoordinates(
+                                cameraPosition!.target.latitude,
+                                cameraPosition!.target.longitude);
+                        Placemark place = placemarks[0];
+                        location =
+                            '${place.street},${place.subLocality},${place.locality},${place.thoroughfare},';
 
-              //form google_maps_webservice package
-              final plist = GoogleMapsPlaces(
-                apiKey: googleApikey,
-                apiHeaders: await GoogleApiHeaders().getHeaders(),
-                //from google_api_headers package
-              );
-              String placeid = place.placeId ?? "0";
-              final detail = await plist.getDetailsByPlaceId(placeid);
-              final geometry = detail.result.geometry!;
-              final lat = geometry.location.lat;
-              final lang = geometry.location.lng;
-              var newlatlang = LatLng(lat, lang);
-
-              //move map camera to selected place with animation
-              mapController?.animateCamera(CameraUpdate.newCameraPosition(
-                  CameraPosition(target: newlatlang, zoom: 17)));
-            }
-          },
-          child: Padding(
-            padding: EdgeInsets.all(15),
-            child: Card(
-              child: Container(
-                  padding: EdgeInsets.all(0),
-                  width: MediaQuery.of(context).size.width - 40,
-                  child: ListTile(
-                    // onTap: () {
-                    //   print(_addrController.text);
-                    // },
-                    title: Text(
-                      location,
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.search),
-                      onPressed: () {
-                        print(_addrController);
+                        setState(() {
+                          //get place name from lat and lang
+                          // print(address);
+                          _addrController.text = location;
+                        });
                       },
                     ),
-                    dense: true,
-                  )),
-            ),
-          ),
-        ),
-      ),
-      Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          margin: EdgeInsets.only(bottom: 20),
-          child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xfffFFBF00).withOpacity(.6),
-                fixedSize: const Size(250, 60),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Positioned(
+                        //search input bar
+                        top: 10,
+                        child: Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Card(
+                              child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20)),
+                                  padding: EdgeInsets.all(0),
+                                  width: MediaQuery.of(context).size.width - 40,
+                                  child: ListTile(
+                                    // onTap: () {
+                                    //   print(_addrController.text);
+                                    // },
+                                    title: Text(
+                                      location,
+                                      style: TextStyle(fontSize: 18),
+                                    ),
+                                    trailing: IconButton(
+                                      icon: Icon(Icons.search),
+                                      onPressed: () async {
+                                        var place =
+                                            await PlacesAutocomplete.show(
+                                                context: context,
+                                                apiKey: googleApikey,
+                                                mode: Mode.overlay,
+                                                types: [],
+                                                strictbounds: false,
+                                                components: [
+                                                  Component(
+                                                      Component.country, 'ae')
+                                                ],
+                                                //google_map_webservice package
+                                                onError: (err) {
+                                                  print(err);
+                                                });
+
+                                        if (place != null) {
+                                          setState(() {
+                                            location =
+                                                place.description.toString();
+                                            _addrController.text = location;
+                                          });
+
+                                          //form google_maps_webservice package
+                                          final plist = GoogleMapsPlaces(
+                                            apiKey: googleApikey,
+                                            apiHeaders: await GoogleApiHeaders()
+                                                .getHeaders(),
+                                            //from google_api_headers package
+                                          );
+                                          String placeid = place.placeId ?? "0";
+                                          final detail = await plist
+                                              .getDetailsByPlaceId(placeid);
+                                          final geometry =
+                                              detail.result.geometry!;
+                                          final lat = geometry.location.lat;
+                                          final lang = geometry.location.lng;
+                                          var newlatlang = LatLng(lat, lang);
+
+                                          //move map camera to selected place with animation
+                                          mapController?.animateCamera(
+                                              CameraUpdate.newCameraPosition(
+                                                  CameraPosition(
+                                                      target: newlatlang,
+                                                      zoom: 17)));
+                                        }
+                                      },
+                                    ),
+                                    dense: true,
+                                  )),
+                            ))),
+                    Center(
+                      //picker image on google map
+                      child: Image.asset(
+                        "assets/user_loc.png",
+                        width: 50,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              onPressed: () async {
-                await FirebaseFirestore.instance
-                    .collection("storeowners")
-                    .doc(FirebaseAuth.instance.currentUser!.uid)
-                    .update({"address": _addrController.text}).then((value) {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (builder) => UserStatus()));
-                });
-              },
-              child: Text("Create Profile")),
-        ),
-      )
-    ]));
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Color(0xfffFFBF00).withOpacity(.6),
+                      fixedSize: Size(200, 40),
+                      shape: StadiumBorder()),
+                  onPressed: () async {
+                    FirebaseFirestore.instance
+                        .collection("storeowners")
+                        .doc(FirebaseAuth.instance.currentUser!.uid)
+                        .update({"address": _addrController.text}).then(
+                            (value) {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => UserStatus(),
+                        ),
+                      );
+                    });
+                  },
+                  child: const Text('Pick'),
+                ),
+              ),
+            ],
+          );
+  }
+
+  void getLatLong() async {
+    setState(() {
+      _isLoading = true;
+    });
+    latlong = await getLocation().getLatLong();
+    setState(() {
+      latlong;
+      _isLoading = false;
+    });
   }
 }
--- lib/splash/splash.dart
+++ lib/splash/splash.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:osar_store/auth/main_auth.dart';
+import 'package:osar_store/mainscreen/dashboard/main_dashborad.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -15,10 +17,17 @@ class _SplashState extends State<Splash> {
   @override
   void initState() {
     super.initState();
-    Timer(
-        Duration(seconds: 3),
-        () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => MainAuth())));
+    FirebaseAuth.instance.userChanges().listen((User? user) {
+      if (user != null) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (builder) => MainDashboard()));
+      } else {
+        Timer(
+            Duration(seconds: 3),
+            () => Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => MainAuth())));
+      }
+    });
   }
 
   @override
